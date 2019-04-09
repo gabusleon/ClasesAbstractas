ca9e0031deb95633581e8fcf8a82194f92b50ccc
Gabriel <gabrielleonp@hotmail.com>
2019-04-09 09:48:05 -0500

    Creacion del proyecto con su estructura basica

---
+++ src/ec/edu/ups/clases/EmpleadoPorHoras.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class EmpleadoPorHoras extends Empleado{
+
+    @Override
+    public double calcularSalarioFinal() {
+        /* si el empleado tiene mas de 5 años en la empresa
+        *  a su salario base se le aumenta 50 dolares por cada año extra
+        *  caso contrario su salario final es su salario base
+        */
+        
+    }
+    
+}
